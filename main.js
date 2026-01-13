df58865e679ca9786529c19f74db8281fed775d7
Kajanthan <kajanthan2k1@gmail.com>
2026-01-14 02:23:12 +0530

    Refactor main.js to improve code organization and add CORS middleware

--- main.js
+++ main.js
@@ -1,20 +1,27 @@
-import express from 'express';
-import userRouter from './routes/user.route.js';
-import connectDB from './lib/db.js';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+import connectDB from "./lib/db.js";
 
-const app= express()
-const PORT  = 4000;
+dotenv.config();
+
+const app = express();
+const PORT = 4000;
+
+//CORS middleware - allow requests from React Native
+app.use(cors());
 
 //data understanding middleware
 app.use(express.json());
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }));
 
-//database 
+//database
 connectDB();
 
-app.get('/',(req,res)=>{
-    res.json({msg:"hellow world"})
-})
+app.get("/", (req, res) => {
+  res.json({ msg: "hellow world" });
+});
 
 app.use("/user", userRouter);
 
